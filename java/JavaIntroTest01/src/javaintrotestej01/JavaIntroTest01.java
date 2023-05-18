b61c5a2ab4b6cd6c4bbff9044a93cfd6e66a2c33
jalmendra99 <jalmendra99+github@gmail.com>
2023-05-18 16:27:47 -0300

    Added java exercises

---
+++ java/JavaIntroTest01/src/javaintrotestej01/JavaIntroTest01.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaintrotestej01;
+
+/**
+ *
+ * @author jalm
+ */
+public class JavaIntroTest01 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Hola mundo.");
+        System.out.print("Hola "); // Escribir Sin Saltar
+        
+        System.out.print("Mundo."); // Escribir Sin Saltar
+        
+        int numero = 20;
+        System.out.println();
+        System.out.println("El valor del numero es: " + numero); // Mostramos el valor de numero
+       
+        
+    }
+    
+}
