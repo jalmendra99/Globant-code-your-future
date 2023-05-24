b8a8abb3eb0684bcde3e847714275d5e6d5788dc
jalmendra99 <jalmendra99+github@gmail.com>
2023-05-24 08:31:31 -0300

    corrections to ex 4

--- java/Ejercicios/src/Ejercicios3Extra/Ej04NumerosRomanos.java
+++ java/Ejercicios/src/Ejercicios3Extra/Ej04NumerosRomanos.java
@@ -17,7 +17,7 @@ public class Ej04NumerosRomanos {
 
     public static void main(String[] args) {
 
-        Scanner leer = new Scanner(System.in);  
+        Scanner leer = new Scanner(System.in);
 
         int numero;
         String romano = "";
@@ -28,45 +28,48 @@ public class Ej04NumerosRomanos {
             System.out.print("para pasarlo a números romanos: ");
             numero = leer.nextInt();
 
-            switch (numero) {
-                case 1:
-                    romano = "I";
-                    break;
-                case 2:
-                    romano = "II";
-                    break;
-                case 3:
-                    romano = "III";
-                    break;
-                case 4:
-                    romano = "IV";
-                    break;
-                case 5:
-                    romano = "V";
-                    break;
-                case 6:
-                    romano = "VI";
-                    break;
-                case 7:
-                    romano = "VII";
-                    break;
-                case 8:
-                    romano = "VIII";
-                    break;
-                case 9:
-                    romano = "IX";
-                    break;
-                case 10:
-                    romano = "X";
-                    break;
-                default:
-                    System.out.println("El número ingresado no es válido.");
-            }
         } while (numero < 1 || numero > 10);
-        
-        // Se imprime por pantalla la información solicitada.
-        System.out.println(numero + " en arábicos, equivale a " + romano + " en números romanos.");
 
+        // Se convierte a número romano.
+        romano = ArabicoARomano(numero);
+
+        // Se imprime por pantalla la información solicitada.
+        if (romano.equals("no es válido")) {
+            System.out.println("Hubo un error al convertír el número ingresado.");
+        } else {
+            System.out.println(numero + " en arábicos, equivale a " + romano + " en números romanos.");
+        }
+
+    }
+
+    // Retorna una cadena que corresponde al equivalente en números romanos
+    // del entero ingresado.    
+    public static String ArabicoARomano(int numeroArabico) {
+
+        switch (numeroArabico) {
+            case 1:
+                return "I";
+            case 2:
+                return "II";
+            case 3:
+                return "III";
+            case 4:
+                return "IV";
+            case 5:
+                return "V";
+            case 6:
+                return "VI";
+            case 7:
+                return "VII";
+            case 8:
+                return "VIII";
+            case 9:
+                return "IX";
+            case 10:
+                return "X";
+            default:
+                return "no es válido.";
+        }
     }
 
 }
