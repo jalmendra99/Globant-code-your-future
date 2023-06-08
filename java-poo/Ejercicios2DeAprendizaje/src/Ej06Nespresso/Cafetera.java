d33f60dc4bacf6006347eb2a23a0ca1541c3938e
jalmendra99 <jalmendra99+github@gmail.com>
2023-06-08 20:15:12 -0300

    resolved ex 6

--- java-poo/Ejercicios2DeAprendizaje/src/Ej06Nespresso/Cafetera.java
+++ java-poo/Ejercicios2DeAprendizaje/src/Ej06Nespresso/Cafetera.java
@@ -1,7 +1,6 @@
 /*
 Java-POO - Ejercicios de aprendizaje.
-Ejercicio # - Página #
-NOMBRE_DE_EJERCICIO
+Ejercicio 6 - Página 28
 
 Fer V - Jalmendra99@gmail.com
 
@@ -12,4 +11,60 @@ package Ej06Nespresso;
 
 public class Cafetera {
 
+    // Atributos
+    private int capacidadMaxima;
+    private int cantidadActual;
+
+    // Constructores
+    public Cafetera() {
+    }
+
+    public Cafetera(int capacidadMaxima, int cantidadActual) {
+        this.capacidadMaxima = capacidadMaxima;
+        this.cantidadActual = cantidadActual;
+    }
+    
+    // Getters y setters
+
+    public int getCapacidadMaxima() {
+        return capacidadMaxima;
+    }
+
+    public void setCapacidadMaxima(int capacidadMaxima) {
+        this.capacidadMaxima = capacidadMaxima;
+    }
+
+    public int getCantidadActual() {
+        return cantidadActual;
+    }
+
+    public void setCantidadActual(int cantidadActual) {
+        this.cantidadActual = cantidadActual;
+    }
+    
+    // Métodos
+    
+    public void llenarCafetera() {
+        cantidadActual = capacidadMaxima;
+    }
+    
+    public void servirTaza(int tamanioTazaVacia) {
+        
+        if (tamanioTazaVacia > cantidadActual) {
+            System.out.println("La taza no está llena, solo tiene " + cantidadActual);
+            cantidadActual = 0;
+        } else {
+            cantidadActual -= tamanioTazaVacia;
+            System.out.println("La taza está llena. Contiene " + tamanioTazaVacia);
+        }
+    }
+    
+    public void vaciarCafetera() {
+        cantidadActual = 0;
+    }
+    
+    public void agregarCafe(int cantidadDeCafe) {
+        cantidadActual += cantidadDeCafe;
+    }
+    
 }
