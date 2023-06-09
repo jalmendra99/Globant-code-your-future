857221dad1b647ba9767d01f9224873cfb73b313
jalmendra99 <jalmendra99+github@gmail.com>
2023-06-09 08:44:09 -0300

    minor corrections to ex7

--- java-poo/Ejercicios2DeAprendizaje/src/Ej07Persona/main.java
+++ java-poo/Ejercicios2DeAprendizaje/src/Ej07Persona/main.java
@@ -11,7 +11,8 @@ public class main {
 
     public static void main(String[] args) {
 
-        // Constante para jugar con distintas cantidades de personas.
+        // La cantidad de personas a crear se guarda en 
+        // una constante para poder jugar con distintas cantidades.
         final int CANTIDAD = 4;
 
         // Se crea un array de Persona
@@ -19,11 +20,12 @@ public class main {
         personas = new Persona[CANTIDAD];
 
         // Las variables para calcular los porcentajes.
-        double porcentajeDebajo = 0;
-        double porcentajeBien = 0;
-        double porcentajeArriba = 0;
-        double porcentajeMayores = 0;
-        double porcentajeMenores = 0;
+        // Guardarán la cantidad de personas...
+        double cantPesoBajo = 0;        // ...debajo de su peso ideal
+        double cantPesoCorrecto = 0;    // ...en su peso correcto
+        double cantPesoSobre = 0;       // ...con sobrepeso
+        double cantMayores = 0;         // ...mayores de edad
+        double cantMenores = 0;         // ...menores de edad
 
         // Se crean "CANTIDAD" de nuevas instancias de "Persona"
         // y se cargan los datos de cada una.
@@ -32,72 +34,71 @@ public class main {
             personas[i].crearPersona();
         }
 
-        // Se ejecutan todos los métodos para cada objeto...
-        // Para cada objeto Persona, se usan los métodos de la clase Persona para...
+        // Se ejecutan todos los métodos de cada Persona. Para...
         for (int i = 0; i < CANTIDAD; i++) {
 
-            // Imprime el nombre de la persona actual para referencia.
+            // ...Imprimir el nombre de la persona actual para referencia.
             System.out.print(personas[i].getNombre() + " ");
 
-            // Se calcula y verifica su IMC.
+            // ...Calcular y verificar su IMC.
             switch (personas[i].calcularIMC()) {
                 case -1:
-                    porcentajeDebajo += 1;
+                    cantPesoBajo += 1;
                     System.out.print("está por debajo del peso ideal ");
                     break;
                 case 0:
-                    porcentajeBien += 1;
+                    cantPesoCorrecto += 1;
                     System.out.print("está en su peso ideal ");
                     break;
                 case 1:
-                    porcentajeArriba += 1;
+                    cantPesoSobre += 1;
                     System.out.print("está por encima de su peso ideal ");
                     break;
                 default:
                     throw new AssertionError();
             }
 
-            // Se verifica si es o no mayor de edad.
+            // ...Verificar si es o no mayor de edad.
             if (personas[i].esMayorDeEdad()) {
                 System.out.println("y es mayor de edad.");
-                porcentajeMayores += 1;
+                cantMayores += 1;
             } else {
                 System.out.println("y NO es mayor de edad.");
-                porcentajeMenores += 1;
+                cantMenores += 1;
             }
 
         }
 
         // Se calculan los porcentajes solicitados.
-        porcentajeDebajo *= 100;
-        porcentajeBien *= 100;
-        porcentajeArriba *= 100;
-        porcentajeMayores *= 100;
-        porcentajeMenores *= 100;
+        cantPesoBajo *= 100;
+        cantPesoCorrecto *= 100;
+        cantPesoSobre *= 100;
+        cantMayores *= 100;
+        cantMenores *= 100;
 
-        porcentajeDebajo /= CANTIDAD;
-        porcentajeBien /= CANTIDAD;
-        porcentajeArriba /= CANTIDAD;
-        porcentajeMayores /= CANTIDAD;
-        porcentajeMenores /= CANTIDAD;
+        cantPesoBajo /= CANTIDAD;
+        cantPesoCorrecto /= CANTIDAD;
+        cantPesoSobre /= CANTIDAD;
+        cantMayores /= CANTIDAD;
+        cantMenores /= CANTIDAD;
 
         // Se muestra la información solicitada por pantalla.
         System.out.println("Los porcentajes solicitados son:");
 
-        // Cuantas están por debajo de su peso.
-        System.out.println("Debajo de su peso ideal: " + porcentajeDebajo + "%");
+        // ...debajo de su peso.
+        System.out.println("Debajo de su peso ideal: " + cantPesoBajo + "%");
 
-        // Cuantas en su peso ideal.
-        System.out.println("En su peso ideal: " + porcentajeBien + "%");
+        // ...en su peso ideal.
+        System.out.println("En su peso ideal: " + cantPesoCorrecto + "%");
 
-        // Cuantos por encima
-        System.out.println("Por encima de su peso ideal: " + porcentajeArriba + "%");
+        // ...sobrepeso.
+        System.out.println("Por encima de su peso ideal: " + cantPesoSobre + "%");
 
-        // Cuantos son mayores de edad.
-        System.out.println("Mayores de edad: " + porcentajeMayores + "%");
+        // ...mayores de edad.
+        System.out.println("Mayores de edad: " + cantMayores + "%");
 
-        // Cuntos son menores.
-        System.out.println("Menores de edad: " + porcentajeMenores + "%");
+        // ...menores.
+        System.out.println("Menores de edad: " + cantMenores + "%");
 
     }
 }
