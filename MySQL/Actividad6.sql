c78547fca79c8dd152761d501585c2a9024da805
jalmendra99 <jalmendra99+github@gmail.com>
2023-09-25 20:33:51 -0300

    MySQL ejerc aprendizaje 2

--- MySQL/Actividad6.sql
+++ MySQL/Actividad6.sql
@@ -1,6 +1,7 @@
 -- Actividad 6 - página 31
 -- Fer Vega - jalmendra99@gmail.com
 --
+-- Correr script superheroes.sql
 
 -- b
 SELECT nombre_real FROM superheroes.personajes;
--- MySQL/Actividad7.sql
+++ MySQL/Actividad7.sql
@@ -1,6 +1,7 @@
 -- Actividad 6 - página 31
 -- Fer Vega - jalmendra99@gmail.com
 --
+-- Correr script superheroes.sql
 
 SELECT * FROM superheroes.personajes 
 ORDER BY inteligencia;
