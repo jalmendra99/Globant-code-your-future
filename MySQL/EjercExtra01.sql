f21510ccd41b6fa3079853511ea04d94aab22059
jalmendra99 <jalmendra99+github@gmail.com>
2023-09-28 20:36:34 -0300

    MySQL ejerc extra 1 completed

--- MySQL/EjercExtra01.sql
+++ MySQL/EjercExtra01.sql
@@ -68,26 +68,85 @@ GROUP BY e.jugador;
 
 
 -- 11. Mostrar el número de jugadores de cada equipo.
-
+SELECT Nombre_equipo, count(codigo)
+FROM jugadores
+GROUP BY Nombre_equipo;
 
 
 -- 12. Mostrar el jugador que más puntos ha realizado en toda su carrera.
+SELECT j.Nombre, count(e.Puntos_por_partido) 
+FROM jugadores j, estadisticas e
+WHERE j.codigo = e.jugador
+GROUP BY j.Nombre
+ORDER BY count(e.Puntos_por_partido) DESC
+LIMIT 1;
 
 
+-- 13. Mostrar el nombre del equipo, conferencia y división 
+-- del jugador más alto de la NBA.
+-- 
+-- Fer: checks..
+-- SELECT Nombre, Altura FROM jugadores ORDER BY Altura DESC;
 
--- 13. Mostrar el nombre del equipo, conferencia y división del jugador más alto de la NBA.
+SELECT j.Nombre, e.Nombre, e.Conferencia, e.Division
+FROM equipos e, jugadores j
+WHERE e.Nombre = j.Nombre_equipo
+AND j.Altura = (
+	SELECT max(Altura) 
+    FROM jugadores
+    )
+;
 
 
+-- 14. Mostrar el partido o partidos (equipo_local, equipo_visitante y diferencia) 
+-- con mayor diferencia de puntos.
+--
+-- Checks: Diferencia de puntos en partidos
+-- SELECT equipo_local, equipo_visitante , abs(puntos_local - puntos_visitante) as 'Diferencia'
+-- FROM partidos
+-- ORDER BY abs(puntos_local - puntos_visitante) DESC;
+-- Resultado:
+-- Timberwolves vs Mavericks - 108 pts
+-- Magic vs Suns             - 108 pts
+-- Lakers vs Pacers          - 108 pts
+-- Pistons vs Knicks         - 108 pts
+--
+-- Mayor diferencia de puntos en partidos
+-- SELECT max(abs(puntos_local - puntos_visitante)) as 'Diferencia'
+-- FROM partidos;
+-- Resultado: 108
+-- 
+SELECT equipo_local, equipo_visitante
+FROM partidos
+WHERE abs(puntos_local - puntos_visitante) = (
+	SELECT max(abs(puntos_local - puntos_visitante)) as 'Diferencia'
+	FROM partidos
+	)
+;
 
--- 14. Mostrar el partido o partidos (equipo_local, equipo_visitante y diferencia) con mayor diferencia de puntos.
-
-
-
--- 15. Mostrar quien gana en cada partido (codigo, equipo_local, equipo_visitante, equipo_ganador), en caso de empate sera null.
-
-
-
-
-
-
-
+-- 15. Mostrar quien gana en cada partido (codigo, equipo_local, equipo_visitante, 
+-- equipo_ganador), en caso de empate sera null.
+--
+-- Checks: codigo y equipos que empataron:
+SELECT codigo, equipo_local, equipo_visitante
+FROM partidos
+WHERE puntos_local = puntos_visitante;
+-- Resultados: Incluyo solo 3 de ejemplo para verificar..
+-- 22 Raptors vs Supersonics
+-- 87 Grizzlies vs Suns
+-- 154 Timberwolves vs Pistons
+--
+-- Checks: codigo y equipos, ganó local:
+SELECT codigo, equipo_local, equipo_visitante
+FROM partidos
+WHERE puntos_local > puntos_visitante;
+-- Resultados: Incluyo solo 3 de ejemplo para verificar..
+-- 3 Raptors vs Clippers
+-- 9 Raptors vs Pistons
+-- 11 Raptors vs Pacers
+-- 
+SELECT codigo, equipo_local, equipo_visitante, 
+	IF(puntos_local = puntos_visitante, null, 
+		IF(puntos_local > puntos_visitante, equipo_local, equipo_visitante)
+	) as 'Equipo ganador'
+FROM partidos;
